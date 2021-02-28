4a38174b4c5cf0629a524b96f01b1c27bc8a75e2
Morgan Skillicorn <hello@morgan.games>
2021-02-28 00:53:37 +0000

    Initial commit

---
+++ Assets/VideoController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using VideoLibrary;
+
+public class VideoController : MonoBehaviour
+{
+    [SerializeField]
+    public InputField iField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        //var service = Client.For(YouTube.Default);
+        //var video = service.GetVideo(iField.text);
+
+        var youTube = YouTube.Default; // starting point for YouTube actions
+        var videoInfos = youTube.GetAllVideosAsync(iField.text).GetAwaiter().GetResult();
+        var maxResolution = videoInfos.First(i => i.Resolution == videoInfos.Max(j => j.Resolution));
+
+        Debug.Log(maxResolution.Uri);
+        gameObject.GetComponent<VideoPlayer>().url = maxResolution.Uri;
+        gameObject.GetComponent<VideoPlayer>().Play();
+    }
+
+    public void Pause()
+    {
+        gameObject.GetComponent<VideoPlayer>().Pause();
+    }
+
+    public void Stop()
+    {
+        gameObject.GetComponent<VideoPlayer>().Stop();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# The-Social-Space
+ DES311 Project by MorganSkilly at Abertay University
